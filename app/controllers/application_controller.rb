5f5ef1a6f06d9d86a7810c032467dff6d2f9accc
arthur.simoes <arthurutschgg@gmail.com>
2023-02-04 17:23:57 -0300

    feat: change name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::API
 
   private
   def not_destroyed(error)
-    render json: {errors: error.record.errors}, status: :unprocessable_etity
+    render json: {errors: error.record.errors}, status: :unprocessable_entity
   end
 end
